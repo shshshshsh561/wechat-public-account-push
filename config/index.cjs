233806e5a21f9b04af2a973c95f691959ffa498f
shshshshsh561 <1422781226@qq.com>
2024-07-10 19:12:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'orOzb6I-Ktt0ks209Kx1kHeaNUcY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'hmNB8rXY901Q0KOuVRbwHqaEcNW_AO78vZyU86ZQULQ',
+      useTemplateId: 'bFs2hjkSTeQJyEIl6l3Be4mwCBaAhAekL6ITzyzz98A',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-21',
       festivals: [
@@ -42,9 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2023-12-01' },
       ],
       IS_SHOW_COLOR: false,
      LITERARY_PREFERENCE: '网络',
@@ -53,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'bFs2hjkSTeQJyEIl6l3Be4mwCBaAhAekL6ITzyzz98A',
 
   CALLBACK_USERS: [
     {
