ec1bc32c63869d6b29355d8f5203ddbe6c3348bc
shshshshsh561 <1422781226@qq.com>
2024-07-10 14:31:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'orOzb6O0nEaqofWvGwpV6UpJUis8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'b-tnsS0fIJgMasnMXCWKrRYsSujbNR_GHcBdeS4mnpo',
+      useTemplateId: 'uLSM8xn6Ej8HMps4wDEGamSCBLuKmPZtopj9xMFcqOE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'b-tnsS0fIJgMasnMXCWKrRYsSujbNR_GHcBdeS4mnpo',
+  CALLBACK_TEMPLATE_ID: 'uLSM8xn6Ej8HMps4wDEGamSCBLuKmPZtopj9xMFcqOE',
 
   CALLBACK_USERS: [
     {
