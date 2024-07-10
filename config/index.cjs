7a2b8b277e940ec137b1430ce9c15661deb1f874
shshshshsh561 <1422781226@qq.com>
2024-07-10 17:19:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'orOzb6I-Ktt0ks209Kx1kHeaNUcY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'moJ5Zgm4t98YYEAt4jYginPrwooIkMVbsFfv5_I4Z7Y',
+      useTemplateId: '4u4R32XfoRJSmbBAx352XAq4yvC9hpyRxXs7zHOYcB4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-21',
       festivals: [
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'moJ5Zgm4t98YYEAt4jYginPrwooIkMVbsFfv5_I4Z7Y',
+  CALLBACK_TEMPLATE_ID: '4u4R32XfoRJSmbBAx352XAq4yvC9hpyRxXs7zHOYcB4',
 
   CALLBACK_USERS: [
     {
